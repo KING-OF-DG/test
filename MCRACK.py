4f3c5846ee2980798336a579ee96716a50a8ef90
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-11 09:04:21 +0600

    Update MCRACK.py

--- MCRACK.py
+++ MCRACK.py
@@ -4,10 +4,10 @@ import os,platform,time
 
 bitt=platform.architecture()[0]
 
-if bitt=="64bit":
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+if bitt=="32bit":
+    os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import MX.mahadi
+    import MXD.mahadi
 
 else:
 
